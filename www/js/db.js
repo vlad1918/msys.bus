cd55948730116b074afb4e296a572d3d3e6b8c38
vlad1918 <dima.vlad@gmail.com>
2015-06-22 14:25:40 +0300

    work in progress

---
+++ www/js/db.js
@@ -0,0 +1,68 @@
+/**
+ * Database for the buses
+ */
+
+
+/*** Constants ***/
+//BusTypeEnum
+MINI_BUS = "MINI_BUS";
+BIG_BUS = "BIG_BUS";
+//WorkDayEnum
+MON_THU = "MON_THU";
+FRIDAY = "FRIDAY";
+//StationEnum
+METRO_SYSTEMS = "METRO_SYSTEMS";
+AUREL_VLAICU = "AUREL_VLAICU";
+PIPERA = "PIPERA";
+
+/*** Create arrays for all available buses ***/
+
+//Buses to work MON-THU
+toWorkMonThu = [
+  {time : "16:40", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "17:05", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "17:30", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "17:45", workDay : MON_THU, isApproximate : false, busType : BIG_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+
+  {time : "26:40", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "27:05", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "27:30", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "27:45", workDay : MON_THU, isApproximate : false, busType : BIG_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "36:40", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "37:05", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "37:30", workDay : MON_THU, isApproximate : false, busType : MINI_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS},
+  {time : "37:45", workDay : MON_THU, isApproximate : false, busType : BIG_BUS, startStation : AUREL_VLAICU, endStation : METRO_SYSTEMS}
+];
+
+
+/*** DB functions ***/
+
+/**
+ * Gets all buses that need to leave after the currentTime
+ * @param currentTime - time in "HH:mm" string format
+ * @param goingToWork - boolean
+ * @param workDay - either MON_THU or FRIDAY
+ * @returns array of objects in the format {time : "", workDay : "", isApproximate : false, busType : "", startStation : "", endStation : ""}
+ */
+function getBuses(currentTime, goingToWork, workDay) {
+	var buses = []; //empty array will be populated below
+	if (goingToWork) {
+		if (workDay == MON_THU) {
+			for ( var i = 0; i < toWorkMonThu.length; i++) {
+				if (toWorkMonThu[i].time > currentTime) {
+					buses.push(toWorkMonThu[i]);
+				}
+			}
+		} else {
+			
+		}
+	} else {
+		if (workDay == MON_THU) {
+			
+		} else {
+			
+		}
+	}
+	
+	return buses;
+}
--- www/js/index.js
+++ www/js/index.js
@@ -16,9 +16,48 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 
+    	//Hide views which are not main
+    	$("#busesView").hide();
+    	$("#selectView").hide();
+    	
+    	//Handler for clicking on btnToWork button 
     	$("#btnToWork").click(function() {
-    		$(".main").hide();
+    		$("#mainView").hide();
+    		$("#busesView").empty();
+    		$("#busesView").show();
+    		
+    		var now = new Date();
+    		var currentTime = now.getHours() + ":" + now.getMinutes();
+    		var workDay = (now.getDay() == 5) ? FRIDAY : MON_THU;
+    		var buses = getBuses(currentTime, true, workDay);
+    		
+    		for ( var i = 0; i < buses.length; i++) {
+//    			$("#busesView").append();
+//    			$("#busesView").append("<div class=\"well well-lg\">" + buses[i].time + "</div>");
+    			$("#busesView").append(createBusView(buses[i]));
+			}    		
     	});
     	
     },
 };
+
+/**
+ * Creates a bus view well
+ */
+function createBusView(bus) {
+	var main = document.createElement("div");
+	main.className = "well"; 
+	
+	var img = document.createElement("img");
+	img.src = "img/mini_bus.png"
+	
+	var time = document.createElement("span");
+	time.className = "big"
+	time.innerHTML = bus.time;	
+		
+		
+	main.appendChild(img);
+	main.appendChild(time);
+	
+	return main;
+}
